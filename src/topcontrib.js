e23afbc60a6f36e9ee8bfbde9cfad4e578b140d8
Helder <he7d3r@gmail.com>
2012-02-14 21:39:45 +0000

    Color-code lines according to who has the top contribution for a page.
    Adapted from
    https://en.wikipedia.org/wiki/User:Ais523/topcontrib.js
    
    Note: this code was originally hosted on Portuguese Wikibooks (https://pt.wikibooks.org)

---
+++ src/topcontrib.js
@@ -0,0 +1,33 @@
+/*
+ * This script color-codes lines according to who has the top contribution for a page.
+ * @source: [[w:en:User:Ais523/topcontrib.js]]
+ * @author: [[w:en:User:Ais523]]
+ */
+if (mw.config.get('wgCanonicalSpecialPageName') === 'Contributions') {
+	$(function () {
+		var i, li, a;
+		li = mw.util.$content.find('li');
+		i = -1;
+		a = [];
+		while (++i < li.length) {
+			var s, t;
+			var html = li[i].innerHTML;
+			var match = html.match(/"\/wiki\/([^"]*)"/);
+			if (!match) {
+				match = html.match(/"\/w\/index.php\?title=([^"]*)&amp;redirect=no"/);
+			}
+			t = match[1];
+			if (li[i].getElementsByTagName('strong').length > 0) {
+				s = "#CCCCFF";
+			} else {
+				s = "#FFCCCC";
+			}
+			if (a[t] !== undefined) {
+				s = (a[t] == "#CCCCFF" ? "#CCFFFF" : "#FFFFCC");
+			} else {
+				a[t] = s;
+			}
+			li[i].innerHTML = "<SPAN STYLE='background-color:" + s + "'>" + li[i].innerHTML + "</SPAN>";
+		}
+	});
+}
