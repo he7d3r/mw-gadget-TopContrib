f482e5bf5853823371306adbf6eacb65351d6573
Helder <he7d3r@gmail.com>
2013-06-28 18:29:55 +0000

    Migrate to jQuery and validate with JSHint

--- src/topcontrib.js
+++ src/topcontrib.js
@@ -2,33 +2,42 @@
  * This script color-codes lines according to who has the top contribution for a page.
  * @source: [[w:en:User:Ais523/topcontrib.js]]
  * @author: [[w:en:User:Ais523]]
+ * @author: [[User:Helder.wiki]]
  * @tracking: [[Special:GlobalUsage/User:Helder.wiki/Tools/topcontrib.js]] ([[File:User:Helder.wiki/Tools/topcontrib.js]])
  */
-if (mw.config.get('wgCanonicalSpecialPageName') === 'Contributions') {
-	$(function () {
-		var i, li, a;
-		li = mw.util.$content.find('li');
-		i = -1;
-		a = [];
-		while (++i < li.length) {
-			var s, t;
-			var html = li[i].innerHTML;
-			var match = html.match(/"\/wiki\/([^"]*)"/);
-			if (!match) {
-				match = html.match(/"\/w\/index.php\?title=([^"]*)&amp;redirect=no"/);
-			}
-			t = match[1];
-			if (li[i].getElementsByTagName('strong').length > 0) {
-				s = "#CCCCFF";
-			} else {
-				s = "#FFCCCC";
-			}
-			if (a[t] !== undefined) {
-				s = (a[t] == "#CCCCFF" ? "#CCFFFF" : "#FFFFCC");
-			} else {
-				a[t] = s;
-			}
-			li[i].innerHTML = "<SPAN STYLE='background-color:" + s + "'>" + li[i].innerHTML + "</SPAN>";
+/*jshint browser: true, camelcase: true, curly: true, eqeqeq: true, immed: true, latedef: true, newcap: true, noarg: true, noempty: true, nonew: true, quotmark: true, undef: true, unused: true, strict: true, trailing: true, maxlen: 120, evil: true, onevar: true */
+/*global jQuery, mediaWiki */
+( function ( mw, $ ) {
+'use strict';
+function updateList(){
+	var a = {};
+	mw.util.$content.find( 'li' ).each( function(){
+		var $this = $(this),
+			link, s, t, match;
+		link = $this.find( '.mw-contributions-title' ).attr( 'href' );
+		match = link.match( /\/wiki\/([^"]*)/ );
+		if ( !match ) {
+			match = link.match(/\/w\/index.php\?title=([^"]*)&redirect=no/);
 		}
-	});
+		t = match[1];
+		if ( $this.find( 'strong' ).length ) {
+			// This is the latest edit
+			s = '#CCF';
+		} else {
+			s = '#FCC';
+		}
+		if ( a[t] !== undefined ) {
+			s = a[t] === '#CCF' ? '#CFF' : '#FFC';
+		} else {
+			a[t] = s;
+		}
+		$this.wrapInner('<span style="background:' + s + '" />');
+	} );
 }
+if ( mw.config.get( 'wgCanonicalSpecialPageName' ) === 'Contributions' ) {
+	mw.loader.using( 'mediawiki.util', function(){
+		$( updateList );
+	} );
+}
+
+}( mediaWiki, jQuery ) );
