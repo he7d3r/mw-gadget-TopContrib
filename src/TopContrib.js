02b8daef8ddc5d512b51905f953f7b9ccbc29df4
Helder <he7d3r@gmail.com>
2014-09-20 11:22:28 -0300

    Remove tracking link (only useful on wiki)

--- src/TopContrib.js
+++ src/TopContrib.js
@@ -4,7 +4,6 @@
  * @author: [[w:en:User:Ais523]]
  * @author: Helder (https://github.com/he7d3r)
  * @license: CC BY-SA 3.0 <https://creativecommons.org/licenses/by-sa/3.0/>
- * @tracking: [[Special:GlobalUsage/User:Helder.wiki/Tools/topcontrib.js]] ([[File:User:Helder.wiki/Tools/topcontrib.js]])
  */
 /*jshint browser: true, camelcase: true, curly: true, eqeqeq: true, immed: true, latedef: true, newcap: true, noarg: true, noempty: true, nonew: true, quotmark: true, undef: true, unused: true, strict: true, trailing: true, maxlen: 120, evil: true, onevar: true */
 /*global jQuery, mediaWiki */
