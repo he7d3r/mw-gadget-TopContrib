efc1d22298f6acfab10329a2c0089c7ff1a489c3
Helder <he7d3r@gmail.com>
2014-09-27 20:49:03 -0300

    Apply MediaWiki Coding Conventions
    Validated with jscs 1.6.2, using preset "wikimedia"

--- src/TopContrib.js
+++ src/TopContrib.js
@@ -6,36 +6,36 @@
  * @license: CC BY-SA 3.0 <https://creativecommons.org/licenses/by-sa/3.0/>
  */
 ( function ( mw, $ ) {
-'use strict';
-function updateList(){
-	var a = {};
-	mw.util.$content.find( 'li' ).each( function(){
-		var $this = $(this),
-			link, s, t, match;
-		link = $this.find( '.mw-contributions-title' ).attr( 'href' );
-		match = link.match( /\/wiki\/([^"]*)/ );
-		if ( !match ) {
-			match = link.match(/\/w\/index.php\?title=([^"]*)&redirect=no/);
-		}
-		t = match[1];
-		if ( $this.find( 'strong' ).length ) {
-			// This is the latest edit
-			s = '#CCF';
-		} else {
-			s = '#FCC';
-		}
-		if ( a[t] !== undefined ) {
-			s = a[t] === '#CCF' ? '#CFF' : '#FFC';
-		} else {
-			a[t] = s;
-		}
-		$this.wrapInner('<span style="background:' + s + '" />');
-	} );
-}
-if ( mw.config.get( 'wgCanonicalSpecialPageName' ) === 'Contributions' ) {
-	mw.loader.using( 'mediawiki.util', function(){
-		$( updateList );
-	} );
-}
+	'use strict';
+	function updateList() {
+		var a = {};
+		mw.util.$content.find( 'li' ).each( function () {
+			var $this = $(this),
+				link, s, t, match;
+			link = $this.find( '.mw-contributions-title' ).attr( 'href' );
+			match = link.match( /\/wiki\/([^"]*)/ );
+			if ( !match ) {
+				match = link.match(/\/w\/index.php\?title=([^"]*)&redirect=no/);
+			}
+			t = match[1];
+			if ( $this.find( 'strong' ).length ) {
+				// This is the latest edit
+				s = '#CCF';
+			} else {
+				s = '#FCC';
+			}
+			if ( a[t] !== undefined ) {
+				s = a[t] === '#CCF' ? '#CFF' : '#FFC';
+			} else {
+				a[t] = s;
+			}
+			$this.wrapInner('<span style="background:' + s + '" />');
+		} );
+	}
+	if ( mw.config.get( 'wgCanonicalSpecialPageName' ) === 'Contributions' ) {
+		mw.loader.using( 'mediawiki.util', function () {
+			$( updateList );
+		} );
+	}
 
 }( mediaWiki, jQuery ) );
